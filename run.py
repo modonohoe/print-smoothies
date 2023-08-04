e325fae22a2df6477f748f343d69434248029437
modonohoe <>
2023-08-04 22:29:52 +0000

    Confirm access for APIs to Google Sheets and define contant variables for this

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Code taken from the 'love_sandwiches' tutorial
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+
+# The scope lists the APIs the program should access in order to run
+# variables whose value does not change are 'constants'
+# constant variable names are written in capital letters
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('print_smoothies')
+
+# accesses names and ingredients of smoothies
+menu = SHEET.worksheet('menu')
+data = menu.get_all_values()
+print(data)
+
+orders = SHEET.worksheet('orders')
+
+regulars = SHEET.worksheet('regulars')
