a004773ce307db92b6a04da3d32fb3800c357983
modonohoe <>
2023-08-05 14:49:24 +0000

    Add welcome message

--- run.py
+++ run.py
@@ -24,3 +24,17 @@ data = menu.get_all_values()
 orders = SHEET.worksheet('orders')
 
 regulars = SHEET.worksheet('regulars')
+
+
+print("Welcome to print(smoothies)!\n")
+print("Dublin's famous on-the-go smoothie bar 🍹 \n")
+print("Location: Dublin City Centre\n")
+print("We are open from Monday to Friday 7am-4pm\n")
+print("Drop in to order one of our speciality smoothies\n")
+print("or order for collection through this terminal!\n")
+print("~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ \n")
+
+print("Please enter one of the following options (1, 2 or 3):")
+print(" 1 = view current menu \n 2 = order \n 3 = set up reocurring order \n")
+
+
